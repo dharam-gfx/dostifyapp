0e5512666cacc38e93e41fce87887d421fb2c85a
kdharmendra <technicalradhe@gmail.com>
2025-05-13 18:22:33 +0530

    feat: update Footer component with new design and privacy message

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,11 @@
 // components/ui/Footer.tsx
 const Footer = () => {
     return (
-      <footer className="bg-gray-800 text-white py-4">
-        <div className="max-w-7xl mx-auto px-4">
-          <div className="flex justify-center">
-            <p>&copy; 2025 DostifyApp. All rights reserved.</p>
-          </div>
-        </div>
-      </footer>
+      <footer className="h-app-footer fixed bottom-0 right-0 left-0 z-10 py-1 flex flex-wrap items-center justify-center w-full mx-auto max-w-2xl">
+      <p className="text-[9px] text-zinc-800 dark:text-zinc-500">
+        messages are end-to-end encrypted and never stored
+      </p>
+    </footer>
     );
   };
   
